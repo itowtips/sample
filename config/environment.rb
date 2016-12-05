f4f85bf58155671e43a928273b5bfc6aacad9da9
itowtips <ito@web-tips.co.jp>
2016-12-05 21:50:19 +0900

    simple chat

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3333']
+
 # Initialize the Rails application.
 Rails.application.initialize!
